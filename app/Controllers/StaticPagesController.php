0c41c46eeabda508705f5b3d97067815eeb180d4
Luan Vitor Simião oliveira <luanv.oliveira@outlook.com>
2021-11-13 20:55:20 -0300

    code format

--- app/Controllers/StaticPagesController.php
+++ app/Controllers/StaticPagesController.php
@@ -6,16 +6,16 @@
  * Date: 06-Aug-17
  * Time: 16:50
  */
+
 namespace App\Controllers;
 
-class StaticPagesController{
-	
-	public static function serveLogin(){
-		 view('login');
+class StaticPagesController {
+
+	public static function serveLogin() {
+		view('login');
 	}
 
-    public static function serveHome(){
-        view('home',['somedata'=>["this","is","awesome"]]);
-    }
-
+	public static function serveHome() {
+		view('home', ['somedata' => ["this", "is", "awesome"]]);
+	}
 }
